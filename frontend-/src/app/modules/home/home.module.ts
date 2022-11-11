a8277bb2cc3ab1ca393473b7aaacadb9b37cca73
David Montoya <deividelpaisa@hotmail.com>
2022-11-11 12:50:12 -0500

    Initial config

---
+++ frontend-/src/app/modules/home/home.module.ts
@@ -0,0 +1,18 @@
+import { HomeRoutingModule } from './home-routing.module';
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+
+
+@NgModule({
+  declarations: [
+  ],
+  imports: [
+    CommonModule,
+    HomeRoutingModule,
+    ReactiveFormsModule,
+  ],
+  providers: [
+  ]
+})
+export class HomeModule { }
